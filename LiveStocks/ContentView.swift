8129ee8c1216b38d36b8a063fd2dadcc6d6ae1ac
kravchenkoalina <alina14022000@gmail.com>
2020-05-23 23:03:40 -0400

    initial project setup

---
+++ LiveStocks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LiveStocks
+//
+//  Created by Alina Kravchenko on 2020-05-23.
+//  Copyright © 2020 LiveStocks. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
